4b53006c6beb9442a10104e838fda5db6873720c
Amr Magdy <amagdy195@yahoo.com>
2019-03-09 15:35:36 +0200

    added pod files

---
+++ Pods/Target Support Files/Pods-automationWorkShopTests/Pods-automationWorkShopTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_automationWorkShopTests : NSObject
+@end
+@implementation PodsDummy_Pods_automationWorkShopTests
+@end
---
+++ Pods/Target Support Files/Pods-automationWorkShopUITests/Pods-automationWorkShopUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_automationWorkShopUITests : NSObject
+@end
+@implementation PodsDummy_Pods_automationWorkShopUITests
+@end
